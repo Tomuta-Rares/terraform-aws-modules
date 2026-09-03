e11ad513b0cfffbeb33287bcbe634edc9646d524
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-09-03 11:17:41 +0300

    change

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -9,3 +9,15 @@ output "cluster_endpoint" {
 output "aws_load_balancer_controller_role_arn" {
   value = aws_iam_role.aws_load_balancer_controller.arn
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
