bf08c32bd963e372beb861938bca86228c7c50bb
Tomuta-Rares <tomuta_rares2010@yahoo.com>
2026-09-03 11:33:55 +0300

    change

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -84,9 +84,9 @@ resource "aws_eks_node_group" "this" {
   instance_types = var.node_instance_types
 
   scaling_config {
-    desired_size = 6
+    desired_size = 5
     min_size     = 1
-    max_size     = 6
+    max_size     = 5
   }
 
   depends_on = [
@@ -157,15 +157,6 @@ resource "aws_eks_addon" "ebs_csi_driver" {
     aws_iam_role_policy_attachment.ebs_csi_driver_policy
   ]
 }
-
-resource "aws_eks_addon" "pod_identity_agent" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "eks-pod-identity-agent"
-
-  depends_on = [
-    aws_eks_node_group.this
-  ]
-}
 ##############################################################################
 
 
@@ -210,73 +201,4 @@ resource "aws_iam_policy" "aws_load_balancer_controller" {
 resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
   role       = aws_iam_role.aws_load_balancer_controller.name
   policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
-}
-
-
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
-resource "aws_iam_policy" "external_secrets" {
-  name = "${var.cluster_name}-external-secrets-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ]
-
-        Resource = [
-          "arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:dev-shopping/*"
-        ]
-      }
-    ]
-  })
-}
-
-
-
-resource "aws_iam_role" "external_secrets" {
-  name = "${var.cluster_name}-external-secrets-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "pods.eks.amazonaws.com"
-        }
-
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "external_secrets" {
-  role       = aws_iam_role.external_secrets.name
-  policy_arn = aws_iam_policy.external_secrets.arn
-}
-
-resource "aws_eks_pod_identity_association" "external_secrets" {
-  cluster_name    = aws_eks_cluster.this.name
-  namespace       = "external-secrets"
-  service_account = "external-secrets"
-  role_arn        = aws_iam_role.external_secrets.arn
-
-  depends_on = [
-    aws_eks_addon.pod_identity_agent,
-    aws_iam_role_policy_attachment.external_secrets
-  ]
 }
